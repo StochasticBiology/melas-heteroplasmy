28dc2d7fe1c1a027ca243eb59ac238cc85d10aa1
jaryaman <j.aryaman25@gmail.com>
2022-03-25 12:49:59 +0000

    doc: demo params

--- src/plot/plot_posterior_and_MAP.py
+++ src/plot/plot_posterior_and_MAP.py
@@ -11,7 +11,7 @@ pos_dir = '../simulate/'
 trunc = 1
 
 
-########### Set these params to mirror what is in ../simulate/met_hast.c ##################
+# TODO: These have been tweaked for "demo" mode. Please see comment for original values
 thin_orig = 1 # was originally 10
 burnin = 0  # 0.5e7/thin_orig # posterior burnin
 thin_again = 1 # was originally 10  # a second round of thinning for the summary stats
