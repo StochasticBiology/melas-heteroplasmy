7485b5b8af96d65d1c28bda4a86c486355aaaaad
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2022-03-28 11:33:59 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ if [[ $commandstr == *--demo* ]]; then
   cd src/simulate
   gcc -o3 met_hast.c -lm -o met_hast.ce -g
   ./met_hast.ce 1 0.1 42
-  cd ../plots
+  cd ../plot
   python3 plot_posterior_and_MAP.py 1
   python3 plot_mcmc_extension.py 1
   python3 fancy_plot_mcmc_extension.py 1
@@ -20,7 +20,7 @@ if [[ $commandstr == *--full* ]]; then
   cd src/simulate
   gcc -o3 met_hast.c -lm -o met_hast.ce -g
   ./met_hast.ce 0 0.1 42
-  cd ../plots
+  cd ../plot
   python3 plot_posterior_and_MAP.py 0
   python3 plot_mcmc_extension.py 0
   python3 fancy_plot_mcmc_extension.py 0
